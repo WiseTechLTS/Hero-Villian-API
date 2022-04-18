dce8b2ff7e45bfa864ae173b4f613169508e0baf
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-18 15:35:10 -0500

    Migrated tables into Database

--- super_types/models.py
+++ super_types/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class SuperType(models.Model):
-    name = models. CharField(max_length=255)
+    name = models.CharField(max_length=255)
     alter_ego = models.CharField(max_length=255)
     primary_ability = models.CharField(max_length=255)
     secondary_ability =  models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+    #super_type = models.ForeignKey(super_types, on_delete=models.CASCADE)
