ce596f708aeacc5edaa35e0de94419b8982a5c0e
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-18 16:20:46 -0500

    updated project urls + cereated first view functions + created supers app url file

--- super_types/views.py
+++ super_types/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperTypeSerializer
+from .models import SuperType
+from super_types import serializers
 
 @api_view(['GET'])
 def super_types_list(request):
+    
+    super_types = SuperType.objects.all()
 
+    serializer = SuperTypeSerializer(super_types, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
