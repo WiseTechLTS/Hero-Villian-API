c3bd6800a1ce1d2c0923aa242d3f41e8985c1301
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-18 18:58:47 -0500

    Created functionality to update a specific supertype user in the super_type file

--- super_types/views.py
+++ super_types/views.py
@@ -1,8 +1,11 @@
+from urllib import response
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperTypeSerializer
 from .models import SuperType
+from super_types import serializers
 
 @api_view(['GET','POST'])
 def super_types_list(request):
@@ -16,13 +19,16 @@ def super_types_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
 @api_view(['GET'])
 def super_types_detail(request, pk):
-    try:
-        super_types = SuperType.objects.get(pk=pk)
-        serializer = SuperTypeSerializer(super_types),
+    if request.method == 'GET':
+        super_types = get_object_or_404(super_types, pk=pk)
+        serializer = SuperType(super_types),
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        super_types = get_object_or_404(SuperType, pk=pk)
+        serializer = SuperTypeSerializer(super_types, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
         
-    except SuperType.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND),
